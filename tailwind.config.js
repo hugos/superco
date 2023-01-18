be4171c166d5861f24829885831cae6dc607090a
Hugo <45741489+hugos@users.noreply.github.com>
2023-01-18 12:41:31 +0000

    Content Cards working. Just need some text config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,27 @@ module.exports = {
     './templates/customers/*.liquid',
   ],
   theme: {
+    fontSize: {
+      'xs': '12px',
+      sm: '14px',
+      'base': '16px',
+      'lg': '18px',
+      'xl': '20px',
+      '2xl': '24px',
+      '3xl': '30px',
+      '4xl': '36px',
+      '5xl': '48px',
+      '6xl': '60px',
+      '7xl': '72px',
+    },
     extend: {},
     fontFamily: {
-      'GT-light': ['GT-Super-Light'],
+      'GTlight': ['GT-Super-Light'],
       'GT-m': ['GT-Super-Medium'],
       'PT-m': ['Platform-Medium'],
+      'PT-r': ['Platform-Regular'],
       'adelia': ['Adelia']
-    }
+    },
   },
   plugins: [],
 }
